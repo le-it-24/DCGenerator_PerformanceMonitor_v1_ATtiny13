dad20c6e012ca2e7eaf06ce993a9b75cde0c44c5
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-20 14:58:57 -0800

    Update Sleepcontrol.h

--- include/Sleepcontrol.h
+++ include/Sleepcontrol.h
@@ -8,4 +8,5 @@
 
 
 /***SLEEP CONTROL APPLICATION VARIABLES***/
-int sleepDuration = 100; // Value sets sleep time duration.
+int sleepDuration = 8; // Value sets sleep time duration,
+                        // It is programmed to about 7.8125 ticks/ms.
