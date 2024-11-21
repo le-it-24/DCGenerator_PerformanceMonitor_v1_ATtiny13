26a03b6a8a62dd065e4621d3281c03a75f64a0b8
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-20 17:22:08 -0800

    Update Sleepcontrol.h

--- include/Sleepcontrol.h
+++ include/Sleepcontrol.h
@@ -10,3 +10,15 @@
 /***SLEEP CONTROL APPLICATION VARIABLES***/
 int sleepDuration = 8; // Value sets sleep time duration,
                         // It is programmed to about 7.8125 ticks/ms.
+                        
+
+
+
+/***SLEEP TIMER (TIMER2) METHODS***/
+void setSystemSleepTimer()
+{
+   // Timer2 setup to wake sytem after sleep duration:
+   TCNT2 = 255 - sleepDuration;
+   TIMSK2 |= (1 << TOIE2);
+   TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20); // Runs timer at CLK_PER/1024.
+}
