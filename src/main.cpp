7971e6a8d355cb389e4f5d452cfadef9a2f05f16
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-20 17:24:49 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -51,16 +51,7 @@ void init_sensePins()
 
 
 
-/***SYSTEM SLEEP FUNCTIONS***/
-// This method programs the system sleep timer:
-void setSystemSleepTimer()
-{
-   // Timer2 setup to wake sytem after sleep duration:
-   TCNT2 = 255 - sleepDuration;
-   TIMSK2 |= (1 << TOIE2);
-   TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20); // Runs timer at CLK_PER/1024.
-}
-
+/***MAIN EXECUTION SETUP***/
 void setup()
 {
   /***HARDWARE MODULES INITIALIZATION***/
@@ -78,5 +69,6 @@ void setup()
 
 void loop() 
 {
-  
+  // Set system to sleep after resolving flags:
+  setSystemSleepTimer();
 }
