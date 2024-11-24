715d4dde6628398cb36cd1486c146a7d11174ad4
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-23 20:00:54 -0800

    Update config_tempSense.h

--- include/TempSense/config_tempSense.h
+++ include/TempSense/config_tempSense.h
@@ -4,3 +4,9 @@
 
 /***TEMPERATURE SENSE SYSTEM I/O DEFINITIONS***/
 #define GEN_TEMP_SENSE          A2
+
+
+
+
+/***TEMPERATURE SENSOR SAMPLING CONFIG DATA***/
+const int overSamplSize_genTemp = 10; // Number of samples taken during an oversample session.
