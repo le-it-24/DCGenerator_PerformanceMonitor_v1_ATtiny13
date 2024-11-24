0e3adeb43bb1a776617a1956366628befdbdd085
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-24 14:12:02 -0800

    Update config_CurrentSense.h

--- include/CurrentSense/config_CurrentSense.h
+++ include/CurrentSense/config_CurrentSense.h
@@ -8,5 +8,8 @@
 
 
 
+/***CURRENT SENSOR SPEC DEFINITIONS***/
+#define CURRENT_SENSOR_SENSITIVITY   66 // Sensitivity of specified current sensor.
+
 /***CURRENT SAMPLING DEFINITIONS***/
 const int current_overSampleSize = 10;
