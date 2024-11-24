13d7d31c62807eeb8c61afb82fed100ca2ab0f6e
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-23 19:43:19 -0800

    Update TempSense.h

--- include/TempSense/TempSense.h
+++ include/TempSense/TempSense.h
@@ -4,3 +4,15 @@
 
 /***GENERATOR TEMPERATURE SENSE APP DEPENDENCIES***/
 #include "C:\Users\EpicsLab\OneDrive\Documents\EpicsLab\ResearchProjects\DCGenerator_PerformanceMonitor_v1_ATtiny13\include\TempSense\config_tempSense.h"
+
+
+
+
+/***GENERATOR TEMPERATURE SENSING METHODS***/
+
+// Read and calculate moving  avg temperature for generator:
+float get_genAvgTemp()
+{
+    // Oversample voltage input and average:
+    
+}
