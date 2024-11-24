9676e0b2f829503b0c2358035397b35e3cc1fd83
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-24 12:54:05 -0800

    Update CurrentSense.h

--- include/CurrentSense/CurrentSense.h
+++ include/CurrentSense/CurrentSense.h
@@ -4,3 +4,19 @@
 
 /***CURRENT SENSE SYSTEM APP DEPENDENCIES***/
 #include "C:\Users\EpicsLab\OneDrive\Documents\EpicsLab\ResearchProjects\DCGenerator_PerformanceMonitor_v1_ATtiny13\include\CurrentSense\config_CurrentSense.h"
+
+
+
+
+/***GENERATOR CURRENT OUTPUT SENSING METHODS***/
+
+float get_genAvgCurrentOut()
+{
+    // Build current sense over sample:
+    uint16_t ovI = 0;
+    for(int s = 0; s < current_overSampleSize; s++)
+    {
+        ovI = analogRead(
+    }
+    
+}
