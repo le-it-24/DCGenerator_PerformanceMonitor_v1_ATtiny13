f1876b16717116b6727dab1c58d59ebe0098111b
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-25 13:50:12 -0800

    Update config_SystemControl.h

--- include/config_SystemControl.h
+++ include/config_SystemControl.h
@@ -3,3 +3,5 @@
 
 
 //***SYSTEM OPERATIONAL PARAMETER DEFINITIONS***/
+#define GEN_OUTPUT_LIM    100.00 // Output wattage limit of the specified generator.
+int sys_mode; // Defines operational mode of PerformanceMonitor.
