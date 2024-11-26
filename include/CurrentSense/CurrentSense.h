25a0dcf7a52a76dbf6643df1e22828f7cebd5098
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-25 18:04:34 -0800

    Update CurrentSense.h

--- include/CurrentSense/CurrentSense.h
+++ include/CurrentSense/CurrentSense.h
@@ -26,18 +26,18 @@ int genOverCurrent()
     float AcsValueF = (2.5 - (ovI * (ADC_VREF / ADC_RES)) )/CURRENT_SENSOR_SENSITIVITY;
     
     // Determine if generator current output is above minimum:
-    if(AcsValueF > GEN_AMP_OUTPUT_MIN)
+    if(AcsValueF < GEN_AMP_OUTPUT_MAX)
     {
         return 0;
     }
     
     // If the below line is reached then the generator 
-    // is in under-current condition.
+    // is in over-current condition.
     return 1;
 }
 
 // Method to immediately check if generator current
-// output amps is above the maximum threshold
+// output amps is below the minimum threshold.
 genOverCurrent()
 float get_genAvgCurrentOut()
 {
