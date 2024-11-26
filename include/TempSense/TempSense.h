65120b2b2a40fcb84685d211b43a428a10efeb6c
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-25 16:40:40 -0800

    Update TempSense.h

--- include/TempSense/TempSense.h
+++ include/TempSense/TempSense.h
@@ -14,7 +14,29 @@
 // operating temperature is below the maximum threshold.
 int genTempGoodMax()
 {
+    // Oversample voltage input and average:
+    uint16_t anTemp = 0; // Analog data accumulator for temp signal.
+    for(int s = 0; s < overSamplSize_genTemp; s++)
+    {
+        anTemp += analogRead(GEN_TEMP_SENSE);
+    }
+    anTemp = anTemp/overSamplSize_genTemp;
     
+    // Convert Analog data to voltage:
+    float vTemp = (ADC_VREF * anTemp)/ADC_RES;
+    
+    // Convert voltage to temperature centigrade:
+    float tempC = ((vTemp * 1000) -500)/10;
+    
+    // Determine if tempC above threshold:
+    if(tempC < GEN_MAX_OP_TEMP)
+    {
+        return 1;
+    }
+    
+    // If line below reached the generator temp was
+    // above max threshold.
+    return 0;
 }
 
 // Method to immediately check if generator current
