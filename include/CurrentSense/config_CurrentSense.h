88280a77e7800d33bfbd87b74b9be27e055a497a
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-25 16:50:15 -0800

    Update config_CurrentSense.h

--- include/CurrentSense/config_CurrentSense.h
+++ include/CurrentSense/config_CurrentSense.h
@@ -3,13 +3,28 @@
 
 
 /***CURRENT SENSE SYSTEM I/0 NDEFINITIONS***/
+
 #define GEN_CURRENT_SENSE           A1
 
 
 
 
 /***CURRENT SENSOR SPEC DEFINITIONS***/
+
 #define CURRENT_SENSOR_SENSITIVITY   0.066 // Sensitivity in Volts of specified current sensor.
 
+
+
+
 /***CURRENT SAMPLING DEFINITIONS***/
+
 const int current_overSampleSize = 10;
+
+
+
+
+/***GENERATOR CURRENT SOURCING PARAMETER DEFINITIONS***/
+
+#define GEN_AMP_OUTPUT_MIN    0.25 // This is the minimum output current required from generator. 
+                                   // This spec takes into consideration the current required for the system control.
+#define GEN_AMP_OUTPUT_MAX    5.00
