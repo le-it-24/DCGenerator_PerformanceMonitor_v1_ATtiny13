043151bcaf2265e61bc9202fc5fa37936e01cbcd
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-25 18:25:31 -0800

    Update VoltOutputMonitor.h

--- include/VOUT_Monitor/VoltOutputMonitor.h
+++ include/VOUT_Monitor/VoltOutputMonitor.h
@@ -44,6 +44,34 @@ int genUnderVoltage()
     return 1;
 }
 
+// Method to immediately check if generator current
+// output voltage is above the maximum threshold.
+int genOverVoltage()
+{
+    // Build sample set of voltage samples:
+    uint16_t av = 0;
+    for(int s = 0; s < samplSize_voltageSensor; s++)
+    {
+        av += analogRead(GEN_VOLT_SENSE);
+        delayMicroseconds(10);
+    }
+    // process AV sample:
+    av = av/samplSize_voltageSensor;
+    // Convert analog data to voltage:
+    float vFilter = (VREF * av)/RES;
+    float vOut = vFilter * VDIVIDER_FACTOR;
+    
+    // Determine if vOut less than min treshold:
+    if(vOut > GEN_MIN_VOUT)
+    {
+        return 0;
+    }
+    
+    // If this line is reached, the output voltage is below
+    // min threshold.
+    return 1;
+}
+
 // Sense and calculate current average
 // voltage output.
 float get_GenAvgVoltOut()
