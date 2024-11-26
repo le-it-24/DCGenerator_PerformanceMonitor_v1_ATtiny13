92d2efffd8e97802cb3017aac1b388bf3970b3d3
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-11-25 18:17:35 -0800

    Update config_VoltOutputMonitor.h

--- include/VOUT_Monitor/config_VoltOutputMonitor.h
+++ include/VOUT_Monitor/config_VoltOutputMonitor.h
@@ -8,10 +8,20 @@
 
 
 /***VOLTAGE OUTPUT MONITOR SYSTEM I/O DEFINITIONS***/
+
 #define GEN_VOLT_SENSE          A0
 
 
 
 
 /***VOLTAGE OUTPUT SENSING CONFIGURATIONS***/
+
 int samplSize_voltageSensor = 10; // Number of samples taken per analysis.
+
+
+
+
+/***GENERATOR VOLTAGE OUTPUT PARAMETER DEFINITIONS***/ 
+
+#define GEN_MIN_VOUT   9.00 // This is the minimum output voltage needed to operate generator system.
+#define GEN_MAX_VOUT   25.00 // This is the maximum output voltage permitted from the generator during operation.
